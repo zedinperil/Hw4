2d84b573f4d00a76206f55cd2d44e402f9ac8838
zedinperil <zedinperil@gmail.com>
2017-03-10 22:43:21 -0600

    LEL

--- queriesmahboy.sql
+++ queriesmahboy.sql
@@ -6,7 +6,7 @@
 #02. A query to retrieve the email(s) of a given person
 	Select e.emailId, e.email, e.personId,p.personCode,p.firstName, p.lastName from Emails e 
     join Person p ON e.personId = p.personId
-    WHERE p.PersonCode = 'redman';
+    where p.personCode='bluman';
 #03. A query to add an email to a specific person
 select * from Emails where personId= 2;
 insert into Emails (emailId, email, personId) values 
@@ -18,9 +18,13 @@ Select * From Emails e
 update Emails set email = 'meh' where emailId=5;
 select * from Emails;
 #05. A query (or series of queries) to remove a given person record
-delete Emails from Emails where personId = 2;
-delete from Person where personId = 2;
-delete from Portfolio where personId = 2;
+delete Emails from Emails where Emails.personId = 4;
+delete Person from Person where Person.personId = 4;
+delete p, a from Portfolio p join Assets a on p.portfolioId=a.portfolioId where p.ownerId = 4;
+delete Portfolio, Assets from Portfolio where Portfolio.managerId = 4;
+delete Portfolio, Assets from Portfolio where Portfolio.beneficiaryId = 4;
+
+
 #06. A query to create a person record
 select * from Person;
 insert into Person (personId, personCode, firstName, lastName, personType, secId, street, city, state, country, zip) values 
@@ -40,17 +44,19 @@ select q.firstName, q.lastName, q.personId, q.personCode, l.assetCode, l.assetNa
  insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(6, 'PT006',1,2,3,30,90.3,1000000,320345,0.06);
 #11. A query to associate a particular asset with a particular portfolio
  insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(1,5,6, 12, 1.15, 18785.15, 109081.20, 17.22);
+ select p.portfolioCode, l.assetCode, l.assetName from Portfolio p join Assets a on p.portfolioId=a.portfolioId join AssetsList l on a.assetListId=l.assetListId;
 #12. A query to find the total number of portfolios owned by each person
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsOwned from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.ownerId where p.ownerId=1;
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsOwned from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.ownerId where p.ownerId=2;
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsOwned from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.ownerId where p.ownerId=3;
+select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsOwned from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.ownerId group by p.ownerId;
 
 #13. A query to find the total number of portfolios managed by each person
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsManaged from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.managerId where p.managerId=1;
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsManaged from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.managerId where p.managerId=2;
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsManaged from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.managerId where p.managerId=3;
+select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsManaged from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.managerId group by p.managerId;
 
 #14. A query to find the total value of all stocks in each portfolio(Hint: you can take the "aggregate of" mathematical expression).
+select p.portfolioCode as portfolio, sum(if(a.assetlistId=l.assetlistId and l.assetType='S',a.assetValue,0)) as TotalStockValues from Portfolio p join Assets a on p.portfolioId=a.portfolioId join AssetsList l on l.assetListId=a.assetListId join Stock s on s.assetListId=l.assetListId group by p.portfolioCode;
 
 #15. A query to detect an invalid distribution of private investment assets 
 #   (that is, a query to add up the stake percentage of each such asset and return a list of investment whose percentage exceeds 100%)
+insert into AssetsList(assetListId, assetCode, assetType, assetName) values(5,'HOME','P','Homeboy Hoagies');
+ insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(10,5,1, 92, 1.15, 18785.15, 109081.20, 17.22);
+ insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(11,5,2, 92, 1.15, 18785.15, 109081.20, 17.22);
+ select p.portfolioCode as portfolio, sum(if(a.assetlistId=l.assetlistId and l.assetType='P',a.assetModifier,0)) as PercentStake, q.personCode as OwnerCode from Portfolio p join Assets a on p.portfolioId=a.portfolioId join AssetsList l on l.assetListId join Person q on q.personId=p.ownerId where l.assetType='P' group by p.portfolioCode having sum(if(a.assetlistId=l.assetlistId and l.assetType='P',a.assetModifier,0))>100;
--- templateformakingbuilder.sql
+++ templateformakingbuilder.sql
@@ -107,7 +107,8 @@ create table Deposit (
  insert into Person(personId, personCode, firstName, lastName, persontype, secId, street, city, state, country, zip) values(1, 'redman','Redmond','Mann','J','sec173','7 Gravel Lane', 'Teufort','AZ','USA','10907');
  insert into Person(personId, personCode, firstName, lastName, persontype, secId, street, city, state, country, zip) values(2, 'bluman','Blutarch','Mann','','','7 Gravel Lane', 'Teufort','AZ','USA','10907');
  insert into Person(personId, personCode, firstName, lastName, persontype, secId, street, city, state, country, zip) values(3, 'grayman','Gray','Mann','E','sec682','Location Unknown','Teufort','AZ','USA','');
- insert into Person(personId, personCode, firstName, lastName, persontype, secId, street, city, state, country, zip) values(4, '','none','','','','','','','','');
+ insert into Person(personId, personCode, firstName, lastName, persontype, secId, street, city, state, country, zip) values(4, 'noemailman','Guywith','Noemails','','','Street of mystery','Lincoln','NE','USA','68510');
+
 
  insert into Emails(emailId, personId, email) values (1,1,'killtheblus@gravelpit.co');
  insert into Emails(emailId, personId, email) values (2,2,'blutarchmann@gannco.com');
@@ -116,10 +117,11 @@ create table Deposit (
   
  insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(1, 'PT001',1,3,3,20,1061.89,123674.20,21237.78,1.9959);
  insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(2, 'PT002',2,3,3,20,966.66,112790.50,19333.23,2.5440);
- insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(3, 'PT003',1,1,4,50, 0.80, 40, 40, 9.0150);
- insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(4, 'PT004',2,3,4, 10, 2, 40, 40, 9.0150);
+ insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(3, 'PT003',1,1,null,50, 0.80, 40, 40, 9.0150);
+ insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(4, 'PT004',2,3,null, 10, 2, 40, 40, 9.0150);
  insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(5, 'PT005',3,3,3,20,128439.51,1579300,568790.15,7.7155);
-
+ insert into Portfolio(portfolioId, portfolioCode, ownerId, managerId, beneficiaryId, fees, commissions, totalValue, sumOfAnnualReturns, aggRisk) values(6, 'PT006',4,4,4,10,100,1000,140,1);
+ 
  insert into AssetsList(assetListId, assetCode, assetType, assetName) values(1,'GRAVEL','S','GravelPit Gravel Pits');
  insert into AssetsList(assetListId, assetCode, assetType, assetName) values(2,'TWOSHORTS','P','Shorts Eatery(Family Owned)');
  insert into AssetsList(assetListId, assetCode, assetType, assetName) values(3,'RIRA','D', 'Roth IRA');
@@ -135,6 +137,8 @@ create table Deposit (
  insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(7,4,5, 100000, 8.35, 2452626, 14593000, 16.81);
  insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(8,3,5, 1200000, 0, 116164.15, 1200000, 9.68);
  insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(9,1,5, 10, 1.15, 15654.29, 90901.00, 17.22);
+ insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(11,1,6, 10, 1.15, 15654.29, 90901.00, 17.22);
+
 
  insert into PrivateInvestment(privateInvestmentId, assetListId, quarterlyDividend, baseRateOfReturn, omegaMeasure, pValue) values(1, 2, 32000, 0.314, 0.25, 909010);
  
@@ -145,36 +149,4 @@ create table Deposit (
  
 
 
-#MAYBE WE NEED SOME FOREIGN KEYS REFERENCING OR MAYBE CONSTRAINTS OR INDEXES
-  ###
-##
-select * from Portfolio;
-select * from Person;
-select * from AssetsList;
-select * from Assets;
-select * from PrivateInvestment;
-select * from Stock;
-select * from Deposit;
-select * from Emails;
-
-#select * from Deposit d join AssetsList l on d.assetCode= l.assetCode;
-#select * from PrivateInvestment p join AssetsList l on p.assetCode= l.assetCode;
-#select * from Stock s join AssetsList l on s.assetCode= l.assetCode;
-#select * from Assets a join AssetsList l on a.assetCode= l.assetCode join Deposit d on d.assetType=l.assetType; 
-#select * from Assets a join Portfolio p on a.portfolioCode=p.portfolioCode where p.ownerCode='redman';
-#12
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsOwned from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.ownerId group by p.ownerId;
-
-#13
-select q.firstName as FirstName, q.lastName as LastName, count(a.assetListId=l.assetListId) as AssetsManaged from Portfolio p  join Assets a on a.portfolioId=p.portfolioId join AssetsList l on l.assetListId=a.assetListId join Person q on q.personId=p.managerId group by p.managerId;
-
-
-#14
-select p.portfolioCode as portfolio, sum(a.assetValue) as TotalStockValues from Portfolio p join Assets a on p.portfolioId=a.portfolioId join AssetsList l on l.assetListId=a.assetListId join Stock s on s.assetListId=l.assetListId group by p.portfolioCode;
 commit;
-
-#15
- insert into AssetsList(assetListId, assetCode, assetType, assetName) values(5,'HOME','P','Homeboy Hoagies');
- insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(10,5,1, 92, 1.15, 18785.15, 109081.20, 17.22);
- insert into Assets(assetId, assetListId, portfolioId, assetModifier, risk, annualReturn, assetValue, returnRate) values(11,5,2, 92, 1.15, 18785.15, 109081.20, 17.22);
- select p.portfolioCode as portfolio, sum(if(a.assetlistId=l.assetlistId and l.assetType='P',a.assetModifier,0)) as PercentStake, q.personCode as OwnerCode from Portfolio p join Assets a on p.portfolioId=a.portfolioId join AssetsList l on l.assetListId join Person q on q.personId=p.ownerId where l.assetType='P' group by p.portfolioCode having sum(if(a.assetlistId=l.assetlistId and l.assetType='P',a.assetModifier,0))>100;
