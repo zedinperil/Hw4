6f66a31c0885f9a4c8d6f8c97dd0b581e8f9ed14
achmelka <chmelkaalex@gmail.com>
2017-03-10 19:27:33 -0600

    more querry

--- queriesmahboy.sql
+++ queriesmahboy.sql
@@ -4,20 +4,20 @@
     
 #02. A query to retrieve the email(s) of a given person
 	Select * From Emails e 
-    join Person p ON e.PersonCode = p.PersonCode
-    WHERE e.PersonCode = '312';
+    join Person p ON e.personId = p.personId
+    WHERE p.PersonCode = 'redman';
 #03. A query to add an email to a specific person
-	select * from Person;
-insert into Emails (EMAILaddress, PersonCode) values ('This is not a real email', 
-  (select PersonCode from Person where PersonCode = '312'));
+select * from Emails where personId= 2;
+insert into Emails (emailId, email, personId) values 
+(5, 'This is not a real email', 
+(select personId from Person where personId = 2));
 select * from Person;  
 #04. A query to change the emaill address of a given record
 select * from Person;
-update Emails set EMAILaddress = 'meh' where PersonCode = '314';
+update Emails set EMAILaddress = 'meh' where PersonId = 2;
 #05. A query (or series of queries) to remove a given person record
-delete from Emails where PersonCode = '312';
-delete from Address where PersonCode = '312';
-delete from Person where PersonCode = '312';
+delete from Emails where personId = 2;
+delete from Person where personId = 2;
 #06. A query to create a person record
 select * from Person;
 insert into Person (name) values ('Ubisoft');
@@ -32,13 +32,14 @@ select * from game;
 
 #10. A query to create a new portfolio record
 
-#11. A query to associate a particular asset with a particular asset with a particular portfolio
+#11. A query to associate a particular asset with a particular portfolio
 
 #12. A query to find the total number of portfolios owned by each person
+	select * from Assets a join Portfolio p on a.portfolioCode=p.portfolioCode where p.ownerCode='redman';
 
 #13. A query to find the total number of portfolios managed by each person
 
-#14. A query to find the total value of all stocks in each portfolio(Hint: you can take the aggregate of mathematical expression).
+#14. A query to find the total value of all stocks in each portfolio(Hint: you can take the "aggregate of" mathematical expression).
 
 #15. A query to detect an invalid distribution of private investment assets 
 #   (that is, a query to add up the stake percentage of each such asset and return a list of investment whose percentage exceeds 100%)
